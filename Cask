01aa5150b5a13b8e0e479ce1625b4e7f706671b0
Sebastian Boehm <sebastian@sometimesfood.org>
2014-03-25 14:54:17 +0100

    Remove cask submodule

--- Cask
+++
@@ -1,8 +0,0 @@
-;; -*- mode: Lisp -*-
-(source gnu)
-(source melpa)
-(depends-on "solarized-theme")
-(depends-on "zenburn-theme")
-(depends-on "haskell-mode")
-(depends-on "magit")
-(depends-on "dired-single")
--- modules/sometimesfood-packages.el
+++ modules/sometimesfood-packages.el
@@ -1,7 +1,20 @@
-(add-to-list 'load-path
-             (expand-file-name "elisp/cask" sometimesfood-base-dir))
-(require 'cask)
-(cask-initialize)
+(require 'package)
 (package-initialize)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+(defun ensure-installed (package)
+  "Check if package is installed, install it if not"
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(ensure-installed 'solarized-theme)
+(ensure-installed 'zenburn-theme)
+(ensure-installed 'haskell-mode)
+(ensure-installed 'magit)
+(ensure-installed 'dired-single)
 
 (provide 'sometimesfood-packages)

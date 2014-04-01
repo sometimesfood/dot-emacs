99e023c5c17b7c4e7ea7de3c07d96a271766c3ff
Sebastian Boehm <sebastian@sometimesfood.org>
2014-04-01 15:22:59 +0200

    Switch from rbenv to chruby

--- modules/sometimesfood-ruby.el
+++ modules/sometimesfood-ruby.el
@@ -1,11 +1,6 @@
 (ensure-installed 'minitest)
-(ensure-installed 'rbenv)
-
 (require 'minitest)
-(require 'rbenv)
 
 (add-hook 'ruby-mode-hook 'minitest-mode)
-(add-hook 'ruby-mode-hook 'global-rbenv-mode)
-(setq rbenv-show-active-ruby-in-modeline nil)
 
 (provide 'sometimesfood-ruby)

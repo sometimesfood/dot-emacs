ff460977fbcb2a7736be449223c447b6c6c03770
Sebastian Boehm <sebastian@sometimesfood.org>
2014-07-15 07:26:36 +0200

    Use F5 as a shortcut for "M-x compile"

--- modules/sometimesfood-keybindings.el
+++ modules/sometimesfood-keybindings.el
@@ -5,6 +5,8 @@
   (setq mac-option-modifier nil))
 
 (global-set-key (kbd "C-c h") 'helm-prelude)
+(global-set-key (kbd "<f5>") 'compile)
+(setq compilation-read-command nil)
 (global-unset-key [(control z)])
 (ffap-bindings)
 

88275f242ef8533bbf28b5855ad0628a4e394f3a
Sebastian Boehm <sebastian@sometimesfood.org>
2014-07-18 20:09:33 +0200

    Add F9 magit-status keybinding

--- modules/sometimesfood-keybindings.el
+++ modules/sometimesfood-keybindings.el
@@ -7,6 +7,7 @@
 (global-set-key (kbd "C-c h") 'helm-prelude)
 (global-set-key (kbd "<f5>") 'compile)
 (setq compilation-read-command nil)
+(global-set-key (kbd "<f9>") 'magit-status)
 (global-unset-key [(control z)])
 (ffap-bindings)
 

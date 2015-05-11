06e04cb071de5e3dcd2f4307ecbf800fa7e31810
Sebastian Boehm <sebastian@sometimesfood.org>
2015-05-11 16:07:25 +0200

    Fix keybinding for dash-at-point

--- modules/sometimesfood-keybindings.el
+++ modules/sometimesfood-keybindings.el
@@ -3,7 +3,7 @@
   (setq mac-command-key-is-meta t)
   (setq mac-command-modifier 'meta)
   (setq mac-option-modifier nil)
-  (global-set-key (kbd "M-f1") 'dash-at-point))
+  (global-set-key (kbd "M-<f1>") 'dash-at-point))
 
 (global-set-key (kbd "C-c h") 'helm-prelude)
 (global-set-key (kbd "<f5>") 'compile)

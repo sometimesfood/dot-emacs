5e260a5986bd0713ef0b8b74555921251fdf37b1
Sebastian Boehm <sebastian@sometimesfood.org>
2015-07-10 15:25:08 +0200

    Add pipes to electric-pair pairs in ruby-mode

--- modules/sometimesfood-ruby.el
+++ modules/sometimesfood-ruby.el
@@ -1,5 +1,7 @@
 (add-hook 'ruby-mode-hook
           (lambda ()
+            (set (make-local-variable 'electric-pair-pairs)
+                 (append electric-pair-pairs '((?\| . ?\|))))
             (set (make-local-variable 'compile-command)
                  "bundle exec --no-color rake test")))
 

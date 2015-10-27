e2e8762ed0113b8decc5807f07efe283fd85143c
Sebastian Boehm <sebastian@sometimesfood.org>
2015-10-27 13:26:21 +0100

    Revert "Temporarily ignore load errors for helm-projectile"
    This reverts commit f12d0b8d40bad0720de36d6258e36e3253c1a622.

--- modules/sometimesfood-helm.el
+++ modules/sometimesfood-helm.el
@@ -2,7 +2,7 @@
 (ensure-installed 'helm-projectile)
 
 (require 'helm-config)
-(ignore-errors (require 'helm-projectile)) ; until projectile #836 is fixed
+(require 'helm-projectile)
 
 ;; source: https://github.com/bbatsov/prelude/blob/master/modules/prelude-helm.el
 (defun helm-prelude ()

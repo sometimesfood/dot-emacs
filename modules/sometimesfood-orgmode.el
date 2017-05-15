71176a87b1d245dc259dd25786d52774ce7e49ec
Sebastian Boehm <sebastian@sometimesfood.org>
2017-05-15 18:12:16 +0200

    Customise reftex

--- modules/sometimesfood-orgmode.el
+++ modules/sometimesfood-orgmode.el
@@ -2,4 +2,14 @@
 (setq org-ellipsis "⤵")
 (setq org-clock-idle-time 10)
 
+;; reftex
+(setq reftex-cite-format
+      '((?\C-m . "\\autocite[]{%l}")
+        (?f . "\\footcite[][]{%l}")
+        (?t . "\\textcite[]{%l}")
+        (?p . "\\parencite[]{%l}")
+        (?o . "\\citepr[]{%l}")
+        (?n . "\\nocite{%l}")))
+(setq reftex-sort-bibtex-matches 'author)
+
 (provide 'sometimesfood-orgmode)

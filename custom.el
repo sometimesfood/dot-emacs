834d722f33fd757736a69478c3a35ad63126d261
Sebastian Boehm <sebastian@sometimesfood.org>
2017-05-19 16:53:04 +0200

    Set epa-pinentry-mode for gpg 2.1 on macOS

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(line-move-visual nil))
+ '(epa-pinentry-mode (if (osx-p) 'loopback nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

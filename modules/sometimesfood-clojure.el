bb5c5cfa2846c4f160d836f71c6cf2460a894ff6
Sebastian Boehm <sebastian@sometimesfood.org>
2018-11-13 19:47:50 +0100

    Disable eldoc-mode, replaced by global-eldoc-mode (Emacs 25.1)

--- modules/sometimesfood-clojure.el
+++ modules/sometimesfood-clojure.el
@@ -1,9 +1,4 @@
-(use-package cider
-  :ensure t
-  :defer t
-  :config
-  (add-hook 'cider-mode-hook #'eldoc-mode))
-
+(use-package cider :ensure t :defer t)
 (use-package clojure-mode :ensure t :defer t)
 
 (provide 'sometimesfood-clojure)

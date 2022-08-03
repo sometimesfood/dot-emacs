4d7e3db64be60b586ecae63d9adc9b7793a5c9d4
Sebastian Boehm <sebastian@sometimesfood.org>
2022-08-03 19:13:23 +0200

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,8 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(epa-pinentry-mode (quote loopback))
+ '(custom-safe-themes
+   '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "84890723510d225c45aaff941a7e201606a48b973f0121cb9bcb0b9399be8cba" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default))
+ '(epg-pinentry-mode 'loopback)
+ '(highlight-parentheses-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
  '(line-move-visual nil)
  '(package-selected-packages
-   (quote
-    (php-mode ibuffer-vc auctex go-mode dired-single ggtags paredit multiple-cursors helm-projectile helm-dash reveal-in-osx-finder which-key dockerfile-mode google-c-style gist cider highlight-parentheses yaml-mode scss-mode coffee-mode haml-mode markdown-mode ledger-mode yasnippet magit haskell-mode zenburn-theme solarized-theme dash-at-point))))
+   '(solarized-theme zenburn-theme yasnippet yaml-mode which-key use-package terraform-mode scss-mode reveal-in-osx-finder plantuml-mode php-mode paredit multiple-cursors markdown-mode magit ledger-mode jinja2-mode ibuffer-vc htmlize highlight-parentheses helm-projectile helm-dash haskell-mode haml-mode google-c-style go-mode gist dumb-jump dockerfile-mode dired-single deadgrep dash-at-point coffee-mode cider auctex)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
